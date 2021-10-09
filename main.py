7e4bc1a62fc86a250f30acc21ccc4b65a28c9dbe
xmichelle26x <40724543+xmichelle26x@users.noreply.github.com>
2021-10-09 02:37:50 -0500

    main monto

---
+++ main.py
@@ -0,0 +1,89 @@
+def pedir_salario():  
+    while(True):
+        try:
+            valor = "{:.2f}".format(float(input()))
+            break
+        except ValueError:
+            print('Error, introduce un numero')
+    return valor
+     
+    
+
+
+def pedir_numero():  
+    while(True):
+        try:
+            numero = int(input())
+            break
+        except ValueError:
+            print('Error, introduce un numero')
+    return numero
+
+
+
+def pedir_mes(): 
+	while(True):
+		try:
+			mes = int(input())
+			if(mes==12 or mes==24 or mes==36 or mes==48):
+				break
+		except ValueError:
+				print('Error, introduce una cantidad de meses de la lista')
+	return mes
+
+		
+
+      
+def calcular_cuota_por_categoria(salario):
+	while True:
+		print ("1. BUENO")
+		print ("2. MALO")
+		print ("3. REGULAR")
+		print("Introduce un numero de categoría: ")
+		categoria = pedir_numero() 
+		cuota = 0
+		if categoria == 1:
+			print ("El % de sueldo DISPONIBLE para cuota es de 50\n")
+			cuota = float(salario) * 0.50
+			break
+		elif categoria == 2:
+			print ("El % de sueldo DISPONIBLE para cuota es de 12,5\n")
+			cuota = float(salario) * 0.125  
+			break
+		elif categoria == 3:
+			print("El % de sueldo DISPONIBLE para cuota es de 25\n")
+			cuota = float(salario) * 0.25
+			break
+		else:
+			print ("Introduce un numero entre 1 y 3")
+	return cuota
+
+
+
+    
+def montoCrédito(cuota, meses):
+    año = meses/12
+    monto = cuota * (1-(1+0.15/12)**(-12*año)) / (0.15/12)
+    return monto    
+    
+    
+
+if __name__ == "__main__": 
+	try:
+		print ("Ingrese el valor de su salario:")
+		salario = pedir_salario()
+		print("\n")
+		
+		cuota = calcular_cuota_por_categoria(salario) 
+		print("\nIngrese la cantidad de meses 12-24-36-48 meses:")
+		meses = pedir_mes()
+		
+		print("\n------------------------------------------------------\n")
+		
+		print("Monto del crédito: {:.2f} USD".format(montoCrédito(cuota, meses)),"\n")
+		print("Cuota: {:.2f} USD".format(cuota),"\n")
+		print("Plazo: ",meses, "meses")
+		
+	except Exception as error:
+		print('error', error)
+	
